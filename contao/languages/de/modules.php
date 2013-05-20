e571b43901c33470aa859071c178be6cca4df385
Tristan Lins <tristan.lins@bit3.de>
2013-05-20 11:30:47 +0200

    Add module translations.

---
+++ contao/languages/de/modules.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Logger bridge for Contao Open Source CMS
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    logger
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['MOD']['logger'] = 'Log System';
